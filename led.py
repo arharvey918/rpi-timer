11a87bcb7d67adbe9bd3f0375782688a1ba65d41
Avy Harvey <psu.ftbl@gmail.com>
2020-12-27 17:19:49 -0500

    Add timer

--- led.py
+++ led.py
@@ -1,46 +1,85 @@
 import RPi.GPIO as GPIO
 import time
 
-reset_pressed = False
+from datetime import datetime
+from datetime import timedelta
+
+# Timer length in seconds
+TIMER_LENGTH = 5
+
+button_pressed = False
 
 # Define a threaded callback function to run in another thread when events are detected  
 def button_callback(channel):
-    global reset_pressed
+    global button_pressed
 
     if GPIO.input(25):     # if port 25 == 1  
         print("Rising edge detected on 25")
-        reset_pressed = True
+        button_pressed = True
     else:                  # if port 25 != 1  
-        print("Falling edge detected on 25")  
+        print("Falling edge detected on 25")
 
-# Setup
+def flicker():
+    for i in range(4):
+        GPIO.output(23,GPIO.HIGH)
+        time.sleep(.25)
+        GPIO.output(23,GPIO.LOW)
+        time.sleep(.25)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 25 to be an input pin and set initial value to be pulled low (off)
-
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
-
-GPIO.add_event_detect(25,GPIO.BOTH,callback=button_callback) # Setup event on pin 25 rising edge
-
-# Main loop
-print("Main loop starting")
-
-try:
-    while not reset_pressed:
-        time.sleep(.1)
-    
-    print("Continuing")
-    GPIO.output(24,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(24,GPIO.LOW)
-
+def tick():
     GPIO.output(23,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(23,GPIO.LOW)  
+    time.sleep(.5)
+    GPIO.output(23,GPIO.LOW)
+    time.sleep(.5)
 
-finally:                   # this block will run no matter how the try block exits  
-    print("Cleaning up")
-    GPIO.cleanup()         # clean up after yourself  
+def complete():
+    GPIO.output(24,GPIO.HIGH)
+
+def start_timer(seconds):
+    # Calculate start time
+    start = datetime.now()
+
+    # Calculate end time
+    end = start + timedelta(seconds=seconds)
+
+    # Flicker to indicate start
+    flicker()
+
+    # Loop until we get to end
+    while end < datetime.now():
+        tick()
+    
+    complete()
+
+if __name__ == "__main__":
+    # Setup
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 25 to be an input pin and set initial value to be pulled low (off)
+
+    GPIO.setup(23,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
+
+    GPIO.add_event_detect(25,GPIO.BOTH,callback=button_callback) # Setup event on pin 25 rising edge
+
+    # Main loop
+    print("Main loop starting")
+
+    try:
+        while not button_pressed:
+            time.sleep(.1)
+        
+        button_pressed = False
+        print("Continuing")
+        start_timer(TIMER_LENGTH)
+
+        print("Press button to stop program")
+        while not button_pressed:
+            time.sleep(1)
+
+    finally:                   # this block will run no matter how the try block exits  
+        print("Cleaning up")
+        GPIO.output(23,GPIO.LOW) 
+        GPIO.output(24,GPIO.LOW)
+        GPIO.cleanup()         # clean up after yourself  
